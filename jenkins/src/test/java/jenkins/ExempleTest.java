61196d3b49f51c327d9d00d1101aec5fd9c55d4c
julien <j.mottez@treeptik.fr>
2015-09-07 13:22:34 +0200

    add test

---
+++ jenkins/src/test/java/jenkins/ExempleTest.java
@@ -0,0 +1,14 @@
+package jenkins;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class ExempleTest {
+
+	@Test
+	public void testFail() {
+		assertTrue(false);
+	}
+	
+}
